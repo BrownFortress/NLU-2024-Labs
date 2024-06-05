aa7c9e825a38594ddcfea0b5ba7fbcb9e188da7d
Juan Camacho Mohedano <jucamohedano@gmail.com>
2024-06-05 16:21:45 +0200

    add logic for training or evaluating

--- exam/studentID_name_surname/LM/part_1/main.py
+++ exam/studentID_name_surname/LM/part_1/main.py
@@ -8,17 +8,29 @@ from utils import *
 from tqdm import tqdm
 
 import copy
+import argparse
 import numpy as np
 import torch.optim as optim
 from functools import partial
 from torch.utils.data import DataLoader
 
 
+def init_args():
+    parser = argparse.ArgumentParser(description="Next token prediction task arguments.")
+    parser.add_argument("-mode", type=str, default='eval', help="Model mode: train or eval")
+    return parser
 
 if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f'The device selected: {device}')
+    model_path = 'bin/best_model.pt'
+
 
     # load data
+    parser = init_args()
+    mode = parser.parse_args().mode
+    print(f'Running script in mode: {mode}. If you desire to change it use the -mode argument, i.e. python main.py -mode eval')
+
     train_raw = read_file("dataset/PennTreeBank/ptb.train.txt")
     dev_raw = read_file("dataset/PennTreeBank/ptb.valid.txt")
     test_raw = read_file("dataset/PennTreeBank/ptb.test.txt")
@@ -64,33 +76,34 @@ if __name__ == "__main__":
     best_model = None
     pbar = tqdm(range(1,n_epochs))
     #If the PPL is too high try to change the learning rate
-    for epoch in pbar:
-        loss = train_loop(train_loader, optimizer, criterion_train, model, clip)
-        if epoch % 1 == 0:
-            sampled_epochs.append(epoch)
-            losses_train.append(np.asarray(loss).mean())
-            ppl_dev, loss_dev = eval_loop(dev_loader, criterion_eval, model)
-            losses_dev.append(np.asarray(loss_dev).mean())
-            pbar.set_description("PPL: %f" % ppl_dev)
-            if  ppl_dev < best_ppl: # the lower, the better
-                best_ppl = ppl_dev
-                best_model = copy.deepcopy(model).to('cpu')
-                patience = 3
-            else:
-                patience -= 1
 
-            if patience <= 0: # Early stopping with patience
-                break # Not nice but it keeps the code clean
+    if mode == 'train':
+        for epoch in pbar:
+            loss = train_loop(train_loader, optimizer, criterion_train, model, clip)
+            if epoch % 1 == 0:
+                sampled_epochs.append(epoch)
+                losses_train.append(np.asarray(loss).mean())
+                ppl_dev, loss_dev = eval_loop(dev_loader, criterion_eval, model)
+                losses_dev.append(np.asarray(loss_dev).mean())
+                pbar.set_description("PPL: %f" % ppl_dev)
+                if  ppl_dev < best_ppl: # the lower, the better
+                    best_ppl = ppl_dev
+                    best_model = copy.deepcopy(model).to('cpu')
+                    model_info = {'state_dict': model.state_dict(), 'lang':lang}
+                    torch.save(model_info, model_path)
+                    patience = 3
+                else:
+                    patience -= 1
 
-    best_model.to(device)
-    final_ppl,  _ = eval_loop(test_loader, criterion_eval, best_model)
-    print('Test ppl: ', final_ppl)
+                if patience <= 0: # Early stopping with patience
+                    break # Not nice but it keeps the code clean
+    else:
+        print("*You are in evaluation mode*")
+        # Load model
+        checkpoint = torch.load(model_path)
+        lang = checkpoint['lang']
+        model = LM_LSTM(emb_size, hid_size, vocab_len, pad_index=lang.word2id["<pad>"]).to(device)
+        model.load_state_dict(checkpoint['state_dict'])
+        final_ppl,  _ = eval_loop(test_loader, criterion_eval, best_model)
 
-    # To save the model
-    path = 'bin/best_model.pt'
-    torch.save(model.state_dict(), path)
-    
-    # To load the model you need to initialize it
-    # model = LM_LSTM(emb_size, hid_size, vocab_len, pad_index=lang.word2id["<pad>"]).to(device)
-    # Then you load it
-    # model.load_state_dict(torch.load(path))
+        print('Test PPL: ', final_ppl)
